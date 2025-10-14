59f8ebc96f20dcf307707830b9af41383cda8fee
GAGANDEEP SINGH <2k23.it1a.2310612@gmail.com>
2025-10-14 20:47:24 +0530

    Add files via upload

---
+++ beel/exp8/simulation/gwd_preview_dc/webcomponentsjs/custom-elements-es5-adapter.js
@@ -0,0 +1,3 @@
+
+(function(){if(!HTMLElement.es5Shimmed&&void 0!==window.Reflect&&void 0!==window.customElements&&!window.customElements.polyfillWrapFlushCallback){var BuiltInHTMLElement=HTMLElement;window.HTMLElement=function HTMLElement(){return Reflect.construct(BuiltInHTMLElement,[],this.constructor)};HTMLElement.prototype=BuiltInHTMLElement.prototype;HTMLElement.prototype.constructor=HTMLElement;HTMLElement.es5Shimmed=!0;Object.setPrototypeOf(HTMLElement,BuiltInHTMLElement)}})();
+
